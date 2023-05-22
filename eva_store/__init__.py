e4e103089c0c34be2d009b62e408c26061e68a74
LivCurtis <133100222+LivCurtis@users.noreply.github.com>
2023-05-22 09:29:46 +0100

    Added project from eva_store

---
+++ eva_store/__init__.py
@@ -0,0 +1,5 @@
+
+__all__ = ["store"]
+
+from .store import *
+
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-#TODO: spit out to 403 errors, add date, project, 'not started' status
+#TODO: spit out to 403 errors, add project
 
 import csv
 import re
 from datetime import datetime
+from eva_store import store
 
 TODAYS_DATE = datetime.today().strftime('%d/%m/%Y')
 RECORDS = []
 
 
 class Record:
-    def __init__(self, number, occurences, variant, instances = None) -> None:
+    def __init__(self, project, number, occurences, variant, instances = None) -> None:
+        self.project = project
         self.number = number
         self.occurences = occurences
         self.variant = variant.upper()
         self.instances = instances if instances else []
 
     def __str__(self):
-        return f"Record - Number: {self.number}, Occurences: {self.occurences}, Variant: {self.variant}, Instances: {self.instances}"
+        return f"Record - Project: {self.project} Number: {self.number}, Occurences: {self.occurences}, Variant: {self.variant}, Instances: {self.instances}"
 
 with open("./input/403.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -24,7 +26,13 @@ with open("./input/403.csv", "r") as csv_file:
 
         variant = row[0].split("@")[1].split(".")[0]
         number = re.search(r"\b(\d+)\b", row[0]).group(1)
-        RECORDS.append(Record(number, row[1], variant, row[2:]))
+        project = ""
+
+        for site in store.ALL_SITES:
+            if site.variant_id == variant.upper():
+                project = site.project_name
+
+        RECORDS.append(Record(project, number, row[1], variant, row[2:]))
 
 
 
@@ -35,7 +43,9 @@ for r in RECORDS:
 with open("./output/403_formatted.csv", "w", newline="") as csv_file:
     csv_writer = csv.writer(csv_file)
     for r in RECORDS:
-        row = ["", "", r.variant, r.number, r.occurences, TODAYS_DATE, r.instances, "", "Not-Started"]
+
+
+        row = ["", r.project, r.variant, r.number, r.occurences, TODAYS_DATE, r.instances, "", "Not-Started"]
 
 
 
